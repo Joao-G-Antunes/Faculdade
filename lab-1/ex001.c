6aa3db390faec7046d61926bfe5010c773d57e12
Joao Gabriel <joaogabrielantunesoliv@gmail.com>
2025-04-03 22:33:53 -0300

    lab 1

---
+++ lab-1/ex001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+      float n1, n2, media;
+      printf("Digite as duas notas do aluno:");
+      scanf("%f %f", &n1, &n2);
+      media = (n1 + n2)/2;
+      printf("A media do aluno e: %.2f", media);
+ return 0;
+
+}	
